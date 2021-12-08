66530637b24a0a7f89a59a7d1ff285aacd8e032a
codeinsack <codeinsack@gmail.com>
2021-12-09 01:21:08 +0300

    init bootstrap 5

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <button
+    type="button"
+    class="btn-close"
+    data-bs-dismiss="alert"
+    aria-label="Close"
+  />
+  <strong>Success!</strong>
+  Bootstrap 5 has now been installed and both the CSS and JavaScript is working.
+</div>
+
+<style>
+</style>
