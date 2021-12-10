72daf9c2342e3a43a089036781c534d2fa66f280
codeinsack <codeinsack@gmail.com>
2021-12-11 00:32:35 +0300

    add navbar layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,8 @@
 <script>
+  import Navbar from "./UI/Navbar.svelte";
 </script>
 
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <button
-    type="button"
-    class="btn-close"
-    data-bs-dismiss="alert"
-    aria-label="Close"
-  />
-  <strong>Success!</strong>
-  Bootstrap 5 has now been installed and both the CSS and JavaScript is working.
-</div>
+<Navbar />
 
 <style>
 </style>
---
+++ src/UI/Navbar.svelte
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a href="#" class="navbar-brand">
+      <img src="images/logo.png" alt="logo" />
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#toggler"
+      aria-controls="toggler"
+      aria-expanded="false"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse" id="toggler">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Language Exchange</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Make a Phrase</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Word Explainer</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Learning Stats</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
